c352dd5d96b44b7592349d9e716d0641733654d2
Kofi Sarfo <kofi65118@gmail.com>
2025-11-23 21:31:39 +0000

    Weather Manager

--- Extreme_Weather-Event-Logger/src/WeatherManager.java
+++ Extreme_Weather-Event-Logger/src/WeatherManager.java
@@ -1,2 +1,65 @@
-public class WeatherManager {
+import java.util.ArrayList;
+
+public class WeatherManager{
+    private ArrayList<Weather> events = new ArrayList<>();
+
+//    WeatherManager Constructor
+    public WeatherManager(){};
+
+    public void addEvent( Weather event){
+        events.add(event);
+    }
+
+    public ArrayList<Weather> getAllEvents(){
+        return events;
+    }
+
+    //Get events by location
+    public ArrayList<Weather> getEventsByLocation(String location){
+        ArrayList<Weather> eventsByLocation = new ArrayList<>();
+        for(Weather i: events){
+            if(i.getLocation().equals(location))
+                eventsByLocation.add(i);
+        }
+
+        if (!eventsByLocation.isEmpty())
+            return eventsByLocation;
+
+        else
+            System.out.println("No events found with this location");
+
+
+    }
+
+
+    // Get HighLight Intensity Events
+    public ArrayList<Weather> getHighLightIntensityEvents(double threshold){
+        ArrayList<Weather> highLightIntensityEvents = new ArrayList<>();
+        for(Weather i: events){
+            if(i.getIntensity().equals(threshold))
+                highLightIntensityEvents.add(i);
+        }
+
+        if (!highLightIntensityEvents.isEmpty())
+            return highLightIntensityEvents;
+
+        else
+            System.out.println("No events found with this location");
+
+    }
+
+    public void sortByDate(){
+
+    }
+
+    public void sortByLocation(){
+
+    }
+
+    public void sortByDuration(){
+
+    }
+
+
+
 }
