90dcb7d31eb3884b52e9eb15cff4a19b0d346efe
Kekeli <agblobik@gmail.com>
2025-11-23 21:25:05 +0000

    gui update

--- Extreme_Weather-Event-Logger/src/WeatherGUI.java
+++ Extreme_Weather-Event-Logger/src/WeatherGUI.java
@@ -1,17 +1,31 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class WeatherGUI {
+
+public class WeatherGUI extends JFrame implements ActionListener {
     private WeatherManager manager;
+    private JButton myButton;
+    private JLabel myLabel;
+
+    myButton = new JButton("Batman");
+    myLabel = new
+
+    JLabel("Hello, GUI!") {
+
+    }
 
     public WeatherGUI(WeatherManager manager){
         this.manager=manager;
     }
     public void showMainWindow(){
+        super("Weather Event Manager");
         System.out.println("tbd");
     }
-    public void showMainWindow(){
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
