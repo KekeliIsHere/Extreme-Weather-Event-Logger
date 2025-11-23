7c4e30852dcaf76570b5315e1029dec61daf1ef5
Kekeli <agblobik@gmail.com>
2025-11-23 21:10:51 +0000

    gui update

---
+++ Extreme_Weather-Event-Logger/src/WeatherGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class WeatherGUI {
+    private WeatherManager manager;
+
+    public WeatherGUI(WeatherManager manager){
+        this.manager=manager;
+    }
+    public void showMainWindow(){
+        System.out.println("tbd");
+    }
+    public void showMainWindow(){
+
+    }
+}
