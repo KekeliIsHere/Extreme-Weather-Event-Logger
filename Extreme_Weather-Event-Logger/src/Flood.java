af505d6d45d481cb9a05bdcacea6725eea78792e
Gina Asang Gana <ginagana05@gmail.com>
2025-12-03 20:06:02 +0000

    Update Flood.java

--- Extreme_Weather-Event-Logger/src/Flood.java
+++ Extreme_Weather-Event-Logger/src/Flood.java
@@ -1,14 +1,39 @@
-public class Flood extends Weather{
-    private EventType eventType;
+import java.time.LocalDateTime;
+
+public class Flood extends Weather {
     private double waterLevel;
 
-    public Flood(int eventId, String location, String date, int duration, double intensity, double waterLevel){
-        super(eventId, location, date, duration,intensity);
-        this.eventType=EventType.FLOOD;
+    public Flood(String location,
+                 LocalDateTime startDateTime,
+                 double durationInHours,
+                 int intensity, String cause,
+                 double waterLevel) {
+
+
+        super(EventType.FLOOD,
+                location,
+                startDateTime,
+                durationInHours,
+                intensity,
+                cause);
+
+        setWaterLevel(waterLevel);
+    }
+
+    public double getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(double waterLevel) {
+        if (waterLevel < 0) {
+            throw new IllegalArgumentException("cannot be less than 0");
+        }
         this.waterLevel = waterLevel;
     }
 
-    public double getWaterLevel(){
-        return waterLevel;
+    public String toString() {
+        return super.toString() + ", Water Level = " + getWaterLevel();
     }
+
 }
+
