80174a3c7794ad72e8b2a1b704ea65556007f433
Gina Asang Gana <ginagana05@gmail.com>
2025-12-08 21:23:49 +0000

    Update MainWeather.java

--- Extreme_Weather-Event-Logger/src/MainWeather.java
+++ Extreme_Weather-Event-Logger/src/MainWeather.java
@@ -1,14 +1,12 @@
 public class MainWeather {
     public static void main(String[] args) {
+        // Create the manager that stores all the events
+        WeatherManager manager = new WeatherManager();
 
-     // Create the manager that stores all the events
-            WeatherManager manager = new WeatherManager();
+        // Create the GUI and link it to the manager
+        WeatherGUI gui = new WeatherGUI(manager);
 
-            // Create the GUI and link it to the manager
-            WeatherGUI gui = new WeatherGUI(manager);
-
-            // Show the main window (this builds and displays the GUI)
-            gui.showMainWindow();
+        // Show the main window (this builds and displays the GUI)
+        gui.showMainWindow();
     }
-
 }
